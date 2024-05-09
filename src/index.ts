861b5b4574e81c73925d6c0166f4f3eabf2bd18c
rahul4019 <rahulkashyap4019@gmail.com>
2024-05-09 20:41:07 +0530

    Add user model and db connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import 'dotenv/config'
+
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -17,7 +20,10 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log(`Server running on http://localhost:8080`)
-})
+server.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:8080`);
+});
 
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
