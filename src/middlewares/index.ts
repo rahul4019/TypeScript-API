6c560c62a36becb1f5656904b7f8e9bb96a5efd0
rahul4019 <rahulkashyap4019@gmail.com>
2024-05-12 13:17:33 +0530

    Add isOwner middleware for delete user route

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -25,3 +25,23 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
         return res.sendStatus(400);
     }
 };
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity._id') as string;
+
+        if (!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if (id !== currentUserId.toString()) {
+            return res.sendStatus(403);
+        }
+
+        return next();
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(400);
+    }
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
 import { getAllUsers, deleteUser } from '../controllers/user';
-import { isAuthenticated } from '../middlewares';
+import { isAuthenticated, isOwner } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
-    router.delete('/users/:id', isAuthenticated, deleteUser);
+    router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
 };
